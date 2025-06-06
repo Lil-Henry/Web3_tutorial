2e59210e12e29850851e8fcff82a1ef4ea62f3e5
Lil-Henry <yangxinyu@HenryM3.local>
2025-06-06 15:45:59 +0800

    finish lesson 4

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@chainlink/env-enc").config();
+require("./tasks");
+
+
+
+// 直接从 process.env 访问加密的变量
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1;
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY, PRIVATE_KEY_1],
+      timeout: 100000,
+      chainId: 11155111,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    }
+  }
 };
---
+++ tasks/index.js
@@ -0,0 +1,3 @@
+exports.interactContract = require("./interact-fundme");   
+exports.deployContract = require("./deploy-fundme");
+
